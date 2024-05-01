57a908a641214ed7a4217c46cdcf9c200f464952
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-01 08:36:16 +0200

    M3 + M4

--- main.js
+++ main.js
@@ -4,8 +4,22 @@ let griglia = document.getElementById("grid");
 
 for (let i = 1; i <= 100; i++) {
 
+    let elemento = creaQuadrato(i)
+    griglia.append(elemento);
+}
+
+
+function creaQuadrato(numero) {
     let square = document.createElement("div");
     square.classList.add("square");
-    square.innerText = i;
-    griglia.append(square);
+    square.innerText = numero;
+
+    square.addEventListener("click", function () {
+        console.log("cliccato", this)
+        this.classList.toggle("evidenziata")
+
+    });
+
+    return square;
 }
+
